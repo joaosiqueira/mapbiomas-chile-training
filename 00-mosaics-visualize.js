88cb97ed33a8a381b79dc468c42a8f953e05d572
João Siqueira <joaovsiqueira1@gmail.com>
2023-06-28 12:29:01 -0300

    feature: visualize mosaics

--- 00-mosaics-visualize.js
+++ 00-mosaics-visualize.js
@@ -1,15 +1,17 @@
 //
-var asset = 'projects/mapbiomas-chile/assets/MOSAICS/mosaics-2';
+var asset = 'projects/nexgenmap/MapBiomas2/LANDSAT/ARGENTINA/mosaics-1';
 
 var collection = ee.ImageCollection(asset);
 
 var years = [
-    1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989,
-    1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997,
-    1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
-    2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013,
-    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
-    2022
+    // 1985, 1986, 1987, 1988, 1989,
+    // 1990, 1991, 1992, 1993, 1994,
+    // 1995, 1996, 1997, 1998, 1999, 
+    2000, 2001, 2002, 2003, 2004,
+    2005, 2006, 2007, 2008, 2009,
+    2010, 2011, 2012, 2013, 2014,
+    2015, 2016, 2017, 2018, 2019,
+    2020, 2021, 2022
 ].reverse();
 
 var vis = {
@@ -56,5 +58,5 @@ ui.root.widgets().reset(maps);
 
 ui.root.setLayout(ui.Panel.Layout.Flow('horizontal', true));
 
-maps[0].setCenter(-71.33, -39.27, 3);
+maps[0].setCenter(-69.53, -45.23, 3);
 //
